6ce55230961a8d9ad96f98262228a857971125e6
Zach <zachmatuson@gmail.com>
2022-01-19 10:41:42 -0500

    ci: Test production release (#2)
    * Added docker image
    Start scripts now activate venv
    
    * Added docker build action
    
    * Removed npm ci command
    
    * disabled docker login in semantic release
    
    * ci: trigger build
    
    * Updated semantic release rules
    
    * fix: Made docker image name lowercase
    
    * ci: removed additional docker tags
    
    * ci: publish image to ghcr.io
    
    * ci: Enabled builds on release branch
    ci: Builds now generate release note and create a release

--- scripts/start.dev.sh
+++ scripts/start.dev.sh
@@ -1,6 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+# set current directory to top level
+BASE_DIR="$(dirname "$0")/.."
+cd $BASE_DIR
+BASE_DIR=$(pwd)
+
+# activate venv
+VENV="$BASE_DIR/venv"
+source "$VENV/bin/activate"
+
 # set current directory to src
-SRC="$(dirname "$0")/../src"
+SRC="$BASE_DIR/src"
 cd $SRC
 
 echo "Current path:"
@@ -8,5 +17,4 @@ pwd
 
 WORKERS=2
 ADDRESS='0.0.0.0:5000'
-
 gunicorn --workers $WORKERS --bind=$ADDRESS --name=FlourishAPI 'wsgi:create_gunicorn(c="dev")'

883d149dd0ba59e6013c5b7c10e2196276f4ee85
Exactol <zachmatuson@gmail.com>
2022-01-21 16:12:02 -0500

    chore: Setup connection pooling

--- src/config.py
+++ src/config.py
@@ -3,25 +3,36 @@ import os
 
 class Config:
 	SECRET_KEY = ''
+
 	SQLALCHEMY_DATABASE_URI = ''
 	SQLALCHEMY_ECHO = False
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SQLALCHEMY_ENGINE_OPTIONS = {
+		'pool_size': 5
+	}
+
 	RESTX_ERROR_404_HELP = False
 
 class ProdConfig(Config):
 	FLASK_ENV = 'production'
 	DEBUG = False
 	TESTING = False
+
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
 
 class DevConfig(Config):
 	FLASK_ENV = 'development'
 	DEBUG = True
 	TESTING = True
+
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
 
 class LocalConfig(Config):
 	FLASK_ENV = 'local'
 	DEBUG = True
 	TESTING = True
+
 	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:password@localhost/postgres'
+	SQLALCHEMY_ENGINE_OPTIONS = {
+		'pool_size': 2
+	}

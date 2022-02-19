75ad4c4313c3234158f4fe9aed10b74b7ced9331
David Bicer, Jr <dmb484@drexel.edu>
2022-02-18 20:57:37 -0500

    WIP: New user authentication schema development

---
+++ src/app/common/utils/authentication.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from bcrypt import checkpw, gensalt, hashpw
+from jwt import decode, encode
+
+KEY: str = ""
+
+def decrypt_jwt(token: bytes) -> dict:
+    """
+    Decrypts a JWT into a python dictionary object
+    """
+    return decode(token)
+    
+def encrypt_jwt(data: dict) -> bytes:
+    """
+    Encrypts a python dictionary object into a JWT byte array
+    """
+    return encode(data, KEY)
+
+def hash_password(unhashed_password: str) -> str:
+    """
+    Transforms a plain-text password string into a hashed one
+    """
+    return hashpw(unhashed, gensalt())
+
+def check_password(password: str, hashed_password: str) -> bool:
+    """
+    Checks if an unhashed password matches its hashed counterpart
+    """
+    return checkpw(password, hashed_password)
--- src/app/v1/schemas/users.py
+++ src/app/v1/schemas/users.py
@@ -4,13 +4,15 @@ from app.core.models.user import User
 from app.v1.utils import CamelCaseSchema, DisablePostLoadMixin
 
 class UserSchema(CamelCaseSchema):
-	user_id = fields.Int()
+	user_id = fields.Int(required=False)
 	email = fields.Email(required=True)
+	username = fields.Str(required=True)
 
 	@post_load
 	def make(self, data, **kwargs):
 		return User(**data)
 
 class NewUserSchema(DisablePostLoadMixin, UserSchema):
+	password_hash: str = fields.Str(required=True)
 	class Meta:
-		fields = ('email', )
+		fields = ("email", "username", "password_hash")

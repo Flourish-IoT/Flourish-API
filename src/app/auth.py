e84a5132d9d10d5ed450589c36497e910ae6f862
David Bicer, Jr <dmb484@drexel.edu>
2022-02-20 16:38:03 -0500

    Implemented authentication

--- src/app/auth.py
+++ src/app/auth.py
@@ -1,5 +1,5 @@
+from .common.utils.authentication import check_password, hash_password
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from .models import User
 from . import db
@@ -20,7 +20,7 @@ def login_post():
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
     user = User.query.filter_by(email=email).first()
-    if not user or not check_password_hash(user.password, password):
+    if not user or not check_password(user.password, password):
         flash('Please check your login details and try again.')
         return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
     # if the above check passes, then we know the user has the right credentials
@@ -39,8 +39,9 @@ def signup_post():
     if user: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')
         return redirect(url_for('auth.signup'))
+
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(email=email, name=name, password=hash_password(password))
 
     # add the new user to the database
     db.session.add(new_user)

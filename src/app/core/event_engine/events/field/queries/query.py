9ba9a92d36f316fd3533cbc16dd30e0a88d6f35a
Exactol <zachmatuson@gmail.com>
2022-02-21 15:19:44 -0500

    Made order column optional for value query

--- src/app/core/event_engine/events/field/queries/query.py
+++ src/app/core/event_engine/events/field/queries/query.py
@@ -1,25 +1,25 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 from typing import Any, Type, TypeVar, cast
-from app.core.models import BaseModel, SensorData
+from app.core.models import Device, SensorData
 from sqlalchemy.orm.scoping import ScopedSession
 from sqlalchemy import Column, TIMESTAMP, Integer
 
-whitelisted_tables = [SensorData]
-WhitelistedTable = SensorData
+whitelisted_tables = [SensorData, Device]
+WhitelistedTable = SensorData | Device
 
 class Query(ABC):
-	table: BaseModel
+	table: WhitelistedTable
 	id_column: Column[Integer]
-	time_column: Column[TIMESTAMP]
+	order_column: Column | None
 
-	def __init__(self, table: WhitelistedTable, id_column: int | Column[Integer], time_column: datetime | Column[TIMESTAMP]):
+	def __init__(self, table: WhitelistedTable, id_column: int | Column[Integer], order_column: Any | Column = None):
 		if table not in whitelisted_tables:
 			raise ValueError(f'Table {table} is restricted')
 
-		self.time_column = cast(Column[TIMESTAMP], time_column)
-		self.id_column = cast(Column[Integer], id_column)
 		self.table = table
+		self.id_column = cast(Column[Integer], id_column)
+		self.order_column = cast(Column | None, order_column)
 
 	@abstractmethod
 	def execute(self, id: int, column: Column | Any, session: ScopedSession) -> Any:
--- src/tests/unit/core/event_engine/events/field/queries/test_value_query.py
+++ src/tests/unit/core/event_engine/events/field/queries/test_value_query.py
@@ -4,19 +4,24 @@ from app.core.models import User, Alert, Device, SensorData
 from unittest.mock import MagicMock
 import pytest
 
+from app.core.models.device_state import DeviceStateEnum
+
+# TODO: dont hardcode this test
 class TestValueQuery:
-	@pytest.mark.parametrize('table', [(User, Alert, Device)])
+	@pytest.mark.parametrize('table', [(User, Alert)])
 	def test_ctor(self, table):
 		"""Assert constructor properly raises exceptions"""
 		mock_value = MagicMock()
 		with pytest.raises(ValueError):
 			score_function = ValueQuery(table, mock_value, mock_value)
 
-	@pytest.mark.parametrize('table, id_col, time_col, col, expected', [(SensorData, SensorData.plant_id, SensorData.time, SensorData.temperature, 10.85)])
-	def test_query(self, session, table, id_col, time_col, col, expected):
+	@pytest.mark.parametrize('table, id_col, order_col, col, expected', [
+		(SensorData, SensorData.plant_id, SensorData.time, SensorData.temperature, 10.85),
+		(Device, Device.device_id, None, Device.device_state, DeviceStateEnum.Connected)
+	])
+	def test_query(self, session, table, id_col, order_col, col, expected):
 		"""Assert query properly returns latest value"""
-		query = ValueQuery(table, id_col, time_col)
-		query = ValueQuery(table, id_col, time_col)
+		query = ValueQuery(table, id_col, order_col)
 
 		value = query.execute(1, col, session)
 		assert math.isclose(value, expected, rel_tol=1e-2)

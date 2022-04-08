4bcd84b92a8ef82ff114e5be61e8f963936c2b08
Exactol <zachmatuson@gmail.com>
2022-04-08 13:54:52 -0400

    feat: Added whitelist to dynamic field, added Serializable and SerializableClass base classes

--- src/app/common/schemas/dynamic_field.py
+++ src/app/common/schemas/dynamic_field.py
@@ -1,64 +1,118 @@
 from enum import Enum
-from typing import Dict, Type
-from marshmallow.fields import Field, List as ListField
+from types import NoneType
+from typing import Any, Dict, List, Optional, Tuple, Type
+from marshmallow.fields import Field, Raw, Nested
 from marshmallow import ValidationError, Schema
 from pydoc import locate
 from inspect import isclass
-from app.common.utils import PolymorphicSchema
+
+class Serializable:
+	"""Registers a class containing a Field with DynamicField"""
+	__field__: Type[Field]
+
+	@classmethod
+	def __field_kwargs__(cls) -> Dict[str, Any]:
+		"""Returns the field specific kwargs"""
+		return {}
+
+	def __init_subclass__(cls, **kwargs) -> None:
+		super().__init_subclass__(**kwargs)
+
+		# register all subclasses with the DynamicField
+		if cls.__field__ is None:
+			raise ValueError(f"Can't instantiate class {cls} without __field__ attribute defined")
+		DynamicField.register(cls, cls.__field__, cls.__field_kwargs__())
+
+class SerializableClass:
+	"""Registers a class containing a nested Schema with DynamicField"""
+	__schema__: Type[Schema]
+
+	def __init_subclass__(cls, **kwargs) -> None:
+		super().__init_subclass__(**kwargs)
+
+		# register all subclasses with the DynamicField
+		if cls.__schema__ is None:
+			raise ValueError(f"Can't instantiate class {cls} without __schema__ attribute defined")
+		DynamicField.register(cls, Nested, {'nested': cls.__schema__})
 
 class DynamicField(Field):
-	type_mapping: Dict[Type, Type[Field]] = {**Schema.TYPE_MAPPING, list: ListField}
+	"""Dynamically loads/dumps a field"""
 
-	# TODO: whitelist for allowed types?
-	def __init__(self, *args, **kwargs) -> None:
+	# should list be dynamic?
+	# mapping of qualname to ( Field, field kwargs )
+	type_mapping: Dict[Type, Tuple[Type[Field], Dict[str, Any]]] = {**{mapping: (field, {}) for mapping, field in Schema.TYPE_MAPPING.items()}, NoneType: ( Raw, {} )}
+
+	# mapping of qualname to python type. Used to lookup type when deserializing
+	type_name_mapping: Dict[str, Type] = {mapping.__qualname__: mapping for mapping in type_mapping.keys()}
+
+	field_args = []
+	field_kwargs = {}
+	whitelist: List[Type] | None = None
+
+	def __init__(self, whitelist: Optional[List[Type]] = None, *args, **kwargs) -> None:
 		super().__init__(*args, **kwargs)
+		self.field_args = args
+		self.field_kwargs = kwargs
+		self.whitelist = whitelist
+
+	def is_whitelisted_type(self, _type: Type) -> bool:
+		"""Checks whether or not a field for a type is whitelisted
+
+		Args:
+				_type (Type): Type of object
+
+		Returns:
+				bool: Indicates whether or not type has a field that is whitelisted
+		"""
+		if self.whitelist is None:
+			return True
+
+		return any([issubclass(_type, whitelisted_class) for whitelisted_class in self.whitelist])
 
 	def _serialize(self, value, attr, obj, **kwargs):
-		# get full qualified object name
-		module = type(value).__module__
+		if not self.is_whitelisted_type(type(value)):
+			raise ValidationError(f'Type {type(value)} is not allowed')
 
-		# TODO: module names for internal types can get pretty long, can we shorten them?
-		if module is None or module == 'builtins':
-			val_type = type(value).__qualname__
-		else:
-			val_type = f'{module}.{type(value).__qualname__}'
+		# use the value type to find the field used to serialize it
+		if type(value) not in self.type_mapping:
+			raise ValidationError(f'No Field registered for type {type(value)}')
 
-		if isinstance(value, Enum):
-			value = value.value
+		field, field_kwargs = self.type_mapping[type(value)]
 
-		# TODO: special field for classes that have a __schema__ property?
 		return {
-			'value': value,
-			'type': val_type
+			'value': field(*self.field_args, **field_kwargs, **self.field_kwargs).serialize(attr, obj, **kwargs),
+			'type': type(value).__qualname__
 		}
 
 	def _deserialize(self, value, attr, data, **kwargs):
 		if 'value' not in value:
 			raise ValidationError(f'Dynamic field requires a value')
 
+		# get the type associated with the field
 		val_type = value.get('type')
 		if val_type is None:
 			raise ValidationError(f'Dynamic field requires a type')
 
-		# special case for None
-		if val_type == 'NoneType':
-			return None
+		_type = self.type_name_mapping.get(val_type)
+		if _type is None:
+			raise ValidationError(f'No Field registered for type {val_type}')
 
-	# TODO: is this a security vulnerability? Not sure if it loads arbitrary modules
-		cls = locate(val_type)
-		if not isclass(cls):
-			raise ValidationError(f'Invalid type')
+		if not self.is_whitelisted_type(_type):
+			raise ValidationError(f'Type {_type} is not allowed')
 
-		return cls(value['value'])
+		# use type to lookup field
+		field, field_kwargs = self.type_mapping[_type]
+
+		# use field to deserialize value
+		return field(*self.field_args, **field_kwargs, **self.field_kwargs).deserialize(value['value'], 'value', value, **kwargs)
 
 	@classmethod
-	def register(cls, t: Type, field: Type[Field]):
+	def register(cls, _type: Type, field: Type[Field], field_kwargs: Dict[str, Any] = {}):
 		"""All classes that can be loaded by the dynamic field must be registered first
 
 		Args:
 				type (Type): Class type
 				schema (Type[Field]): Field to use when serializing/deserializing class
 		"""
-		# if t.__name__ in cls.type_schemas:
-		# 	raise ValueError(f'Class has already been registered: {{{t.__name__}: {cls.type_schemas[t.__name__]}}}')
-		cls.type_mapping[t.__name__] = field
+		cls.type_mapping[_type] = (field, field_kwargs)
+		cls.type_name_mapping[_type.__qualname__] = _type
--- src/tests/unit/common/schemas/test_dynamic_field.py
+++ src/tests/unit/common/schemas/test_dynamic_field.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 from typing import Any
-from app.common.schemas.dynamic_field import DynamicField
-from app.common.utils import PolymorphicSchema, Serializable
+from app.common.schemas import DynamicField, SerializableClass
+from app.common.utils import PolymorphicSchema
 from app.core.event_engine.post_process_functions import ValueRating
-from marshmallow import fields, post_load, Schema
+from marshmallow import fields, post_load, Schema, ValidationError
 import pytest
 
 class FooSchema(Schema):
-	foo = DynamicField()
+	foo = DynamicField(allow_none=True)
 
 	@post_load
 	def make(self, data, **kwargs):
@@ -17,6 +17,28 @@ class FooSchema(Schema):
 @dataclass
 class Foo():
 	foo: Any
+
+class BazSchema(Schema):
+	baz = DynamicField(allow_none=True)
+@dataclass
+class Baz(SerializableClass):
+	__schema__ = BazSchema
+	baz: Any
+
+class Baz2Schema(BazSchema):
+	bar = DynamicField()
+@dataclass
+class Baz2(Baz):
+	__schema__ = Baz2Schema
+	bar: Any
+
+@dataclass
+class UnregisteredClass:
+	bar: int
+
+@dataclass
+class Bar:
+	a: Any
 
 class TestDynamicField:
 	@pytest.mark.parametrize('value, expected', [
@@ -50,22 +72,54 @@ class TestDynamicField:
 				'type': 'str'
 			}
 		}),
-		# (Foo(foo=datetime(1, 1, 1, 1)), {
-		# 	'foo': {
-		# 		'value': None,
-		# 		'type': 'datetime.datetime'
-		# 	}
-		# }),
+		(Foo(foo=datetime(1, 1, 1, 1)), {
+			'foo': {
+				'value': '0001-01-01T01:00:00',
+				'type': 'datetime'
+			}
+		}),
 		(Foo(foo=ValueRating.High), {
 			'foo': {
-				'value': 4,
-				'type': 'app.core.event_engine.post_process_functions.target_value_score.ValueRating'
+				'value': 'High',
+				'type': 'ValueRating'
 			}
 		}),
 	])
 	def test_dump(self, value, expected):
 		res = FooSchema().dump(value)
 		assert res == expected
+
+	@pytest.mark.parametrize('value', [
+		(Foo(foo=UnregisteredClass(bar=2))),
+	])
+	def test_dump_raises(self, value):
+		with pytest.raises(ValidationError):
+			FooSchema().dump(value)
+
+	@pytest.mark.parametrize('value, whitelist', [
+		(Bar(a=3), [int]),
+		(Bar(a=3), [int, float, str]),
+		(Bar(a=ValueRating.High), [ValueRating]),
+		(Bar(a=ValueRating.High), [int]),
+		(Bar(a=Baz(baz=3)), [Baz]),
+		(Bar(a=Baz2(baz=3, bar='foo')), [Baz]),
+	])
+	def test_dump_whitelist(self, value, whitelist):
+		class BarSchema(Schema):
+			a = DynamicField(whitelist)
+		BarSchema().dump(value)
+
+	@pytest.mark.parametrize('value, whitelist', [
+		(Bar(a=5), [str]),
+		(Bar(a='foo'), [int, float, datetime]),
+		(Bar(a=ValueRating.High), [str]),
+	])
+	def test_dump_whitelist_raises(self, value, whitelist):
+		class BarSchema(Schema):
+			a = DynamicField(whitelist)
+
+		with pytest.raises(ValidationError):
+			BarSchema().dump(value)
 
 	@pytest.mark.parametrize('value, expected', [
 		({
@@ -98,19 +152,131 @@ class TestDynamicField:
 				'type': 'str'
 			}
 		}, Foo(foo='abc')),
-		# (Foo(foo=datetime(1, 1, 1, 1)), {
-		# 	'foo': {
-		# 		'value': None,
-		# 		'type': 'datetime.datetime'
-		# 	}
-		# }),
 		({
 			'foo': {
-				'value': 4,
-				'type': 'app.core.event_engine.post_process_functions.target_value_score.ValueRating'
+				'value': '0001-01-01T01:00:00',
+				'type': 'datetime'
+			}
+		}, Foo(foo=datetime(1, 1, 1, 1))),
+		({
+			'foo': {
+				'value': 'High',
+				'type': 'ValueRating'
 			}
 		}, Foo(foo=ValueRating.High)),
 	])
 	def test_load(self, value, expected):
 		res = FooSchema().load(value)
 		assert res == expected
+
+	@pytest.mark.parametrize('value', [
+		({
+			'foo': {
+				'value': 'bar',
+				'type': 'BadType'
+			}
+		}),
+		({
+			'foo': {
+				'type': 'int'
+			}
+		}),
+		({
+			'foo': {
+				'value': 'MissingType'
+			}
+		}),
+		({
+			'foo': {
+				'value': 'BadValue',
+				'type': 'int'
+			}
+		}),
+	])
+	def test_load_raises(self, value):
+		with pytest.raises(ValidationError):
+			FooSchema().load(value)
+
+	@pytest.mark.parametrize('value, whitelist', [
+		({
+			'a': {
+				'value': 5,
+				'type': 'int'
+			}
+		}, [int]),
+		({
+			'a': {
+				'value': 5,
+				'type': 'int'
+			}
+		}, [int, float, str]),
+		({
+			'a': {
+				'value': 'High',
+				'type': 'ValueRating'
+			}
+		}, [ValueRating]),
+		({
+			'a': {
+				'value': 'High',
+				'type': 'ValueRating'
+			}
+		}, [int]),
+		({
+			'a': {
+				'value': {
+					'baz': {
+						'value': 5,
+						'type': 'int'
+					}
+				},
+				'type': 'Baz'
+			}
+		}, [Baz]),
+		({
+			'a': {
+				'value': {
+					'baz': {
+						'value': 5,
+						'type': 'int'
+					},
+					'bar': {
+						'value': 5,
+						'type': 'int'
+					}
+				},
+				'type': 'Baz2'
+			}
+		}, [Baz]),
+	])
+	def test_load_whitelist(self, value, whitelist):
+		class BarSchema(Schema):
+			a = DynamicField(whitelist)
+		BarSchema().load(value)
+
+	@pytest.mark.parametrize('value, whitelist', [
+		({
+			'a': {
+				'value': 5,
+				'type': 'int'
+			}
+		}, [str]),
+		({
+			'a': {
+				'value': 'foo',
+				'type': 'str'
+			}
+		}, [int, float, datetime]),
+		({
+			'a': {
+				'value': 'High',
+				'type': 'ValueRating'
+			}
+		}, [str]),
+	])
+	def test_load_whitelist_raises(self, value, whitelist):
+		class BarSchema(Schema):
+			a = DynamicField(whitelist)
+
+		with pytest.raises(ValidationError):
+			BarSchema().load(value)

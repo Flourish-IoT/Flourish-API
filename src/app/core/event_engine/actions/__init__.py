941ad5a86be5da0fd32e3119e8513e8b94b0da6c
Zach <zachmatuson@gmail.com>
2022-05-23 18:06:01 -0400

    feat: Added HTTP Action (#36)
    * fix: Added support for dynamically serializing dicts
    
    * feat: Added HTTP Action

--- src/app/core/event_engine/actions/__init__.py
+++ src/app/core/event_engine/actions/__init__.py
@@ -1,3 +1,4 @@
 from .action import Action, ActionSchema
 from .generate_alert import GenerateAlertAction, GenerateDeviceAlertAction, GeneratePlantAlertAction
 # from .clear_alert import
+from .http_action import HTTPAction
